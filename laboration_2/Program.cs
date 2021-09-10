8e2568f7488d65c22ccb11c7694b47c316af5906
Philip Sinnott <philipsinnotts@gmail.com>
2021-09-10 17:34:42 +0200

    Added comments

--- laboration_2/Program.cs
+++ laboration_2/Program.cs
@@ -9,8 +9,10 @@ namespace Laboration2
             Console.WriteLine("Laboration 2 in GIK2F7 | Abstraction, encapsulation & interfaces");
             Console.WriteLine("----------------------------------------------------------------\n");
 
+            // Create an array with all shapes
             Shape[] array = new Shape[] { new Circle(2, 5), new Triangle(4.5, 7.5, 2, 5, 7), new Rectangle(3.4, 6) };
 
+            // Loop through the array and display the results from each method through the ToString() method
             foreach (Shape s in array)
             {
                 Console.WriteLine(s.ToString());
