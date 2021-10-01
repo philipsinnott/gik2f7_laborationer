fbb07092adf40b8f12759006b7b5f944955f224b
Philip Sinnott <philipsinnotts@gmail.com>
2021-10-01 11:26:57 +0200

    Startup

---
+++ laboration_4/Laboration_4/Laboration_4/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using Laboration_4.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Laboration_4
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ laboration_4/Laboration_4/Laboration_4/ViewModels/ShellViewModel.cs
@@ -0,0 +1,109 @@
+﻿using Caliburn.Micro;
+using Laboration_4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboration_4.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        private string _firstName = "Philip";
+        private string _lastName;
+        private string _email;
+        private BindableCollection<PersonModel> _people = new BindableCollection<PersonModel>();
+        private PersonModel _selectedPerson;
+
+        public ShellViewModel()
+        {
+            People.Add(new PersonModel { FirstName = "Person 1", LastName = "Corey" });
+            People.Add(new PersonModel { FirstName = "Person 2", LastName = "Jefferson" });
+            People.Add(new PersonModel { FirstName = "Person 3", LastName = "Swagson" });
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+                NotifyOfPropertyChange(() => FirstName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+                NotifyOfPropertyChange(() => LastName);
+                NotifyOfPropertyChange(() => FullName);
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+                NotifyOfPropertyChange(() => Email);
+            }
+        }
+        public string FullName
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+        public BindableCollection<PersonModel> People
+        {
+            get
+            {
+                return _people; 
+            }
+            set
+            {
+                _people = value; 
+            }
+        }
+        public PersonModel SelectedPerson
+        {
+            get
+            {
+                return _selectedPerson; 
+            }
+            set
+            {
+                _selectedPerson = value;
+                NotifyOfPropertyChange(() => SelectedPerson);
+            }
+        }
+        public bool CanClearText(string firstName, string lastName, string email)
+        {
+            if (string.IsNullOrWhiteSpace(firstName) && string.IsNullOrWhiteSpace(lastName) && string.IsNullOrWhiteSpace(email))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public void ClearText(string firstName, string lastName, string email)
+        {
+            FirstName = "";
+            LastName = "";
+            Email = "";
+        }
+    }
+}
